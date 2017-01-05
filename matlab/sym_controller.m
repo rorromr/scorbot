411efc2c88fb6eaba3f182a6ff033e8688b5ac09
Rodrigo Muñoz <rorro.mr@gmail.com>
2017-01-04 21:00:35 -0300

    DSP format for controller design

--- matlab/sym_controller.m
+++ matlab/sym_controller.m
@@ -5,14 +5,15 @@ tau = sym('tau');
 s = sym('s');
 alpha = sym('alpha');
 Ts = sym('Ts');
+w = sym('w'); % w = z^-1
 z = sym('z');
 
 scontroller = kp + ki*1/s + kd*s/(s*tau+1);
-zcontroller = subs(scontroller, s, (2/Ts)*(z-1)/(z+1));
+zcontroller = subs(scontroller, s, (2/Ts)*(1-w)/(1+w));
 
 [znum, zden] = numden(zcontroller);
-znum_coeff = fliplr(eval(feval(symengine,'coeff',znum,z,'All')));
-zden_coeff = fliplr(eval(feval(symengine,'coeff',zden,z,'All')));
+znum_coeff = fliplr(eval(feval(symengine,'coeff',znum,w,'All')));
+zden_coeff = fliplr(eval(feval(symengine,'coeff',zden,w,'All')));
 
 ccode(znum_coeff)
 ccode(zden_coeff)
@@ -20,16 +21,17 @@ ccode(zden_coeff)
 %% Eval
 vkp = 1;
 vki = 0.5;
-vkd = 0.1;
+vkd = 0.8;
 vtau = 0.1;
-vTs = 0.01;
+vTs = 0.1;
 valpha = 2/vTs;
 values = [vkp, vki, vkd, vtau, vTs, valpha];
 svars = [kp, ki, kd, tau, Ts, alpha];
 
 znum_values = double(subs(znum_coeff,svars,values));
 zden_values = double(subs(zden_coeff,svars,values));
-zcontroller_tf1 = tf(znum_values, zden_values, vTs);
+% Flip coeffs for z^-1
+zcontroller_tf1 = tf(fliplr(znum_values), fliplr(zden_values), vTs, 'Variable', 'z^-1');
 
 %% Verify
 [snum, sden] = numden(scontroller);
@@ -40,7 +42,8 @@ sden_values = double(subs(sden_coeff,svars,values));
 scontroller_tf = tf(snum_values, sden_values);
 zcontroller_tf2 = c2d(scontroller_tf,vTs,'tustin');
 
-bode(zcontroller_tf2, zcontroller_tf1)
-legend('Direct','Manual')
+figure;
+bode(zcontroller_tf1, zcontroller_tf2);
+legend('Direct','C2D');
 
 
