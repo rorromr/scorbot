7f791f7a58dae11c42207f0182159657b7fc1af9
Rodrigo Muñoz <rorro.mr@gmail.com>
2017-03-23 18:44:48 -0300

    Haptic test

--- ros/scorbot_haptic_teleop/src/haptic_teleop.cpp
+++ ros/scorbot_haptic_teleop/src/haptic_teleop.cpp
@@ -1,5 +1,7 @@
 #include <ros/ros.h>
 #include <sensor_msgs/JointState.h>
+#include <omni_msgs/OmniButtonEvent.h>
+#include <std_msgs/Float64.h>
 
 namespace scorbot
 {
@@ -33,10 +35,14 @@ namespace scorbot
         HapticTeleop():
         nh_(),
         nh_priv_("~"),
-        name_("haptic_teleop")
+        name_("haptic_teleop"),
+        publish_cmd_(false)
         {
             // Haptic joint states
-            haptic_sub_ = nh_.subscribe("haptic_joint_states", 10, &HapticTeleop::hapticCallback, this);
+            haptic_js_sub_ = nh_.subscribe("haptic_joint_states", 10, &HapticTeleop::hapticJointStateCallback, this);
+            // Haptic buttons
+            haptic_btn_sub_ = nh_.subscribe("haptic_button", 10, &HapticTeleop::hapticButtonStateCallback, this);
+
             // Joint states publisher for shadow robot
             scorbot_shadow_pub_ = nh_.advertise<sensor_msgs::JointState>("shadow_joint_states", 10);
             // Fill joint state message for shadow robot
@@ -51,32 +57,51 @@ namespace scorbot
             scorbot_shadow_js_.name.push_back("pitch");
             scorbot_shadow_js_.name.push_back("roll");
 
+            // Robot command publisher
+            scorbot_cmd_pub_ = nh_.advertise<std_msgs::Float64>("/scorbot/base_controller/command", 10);
+            cmd_.data = 0.0;
+
             // Joint mapping functions
             joint_map_["base"] = boost::make_shared<LinearJointMapping>(1.0, 0.0);
 
         };
 
-        void hapticCallback(const sensor_msgs::JointStateConstPtr& haptic_joint_state)
+        void hapticButtonStateCallback(const omni_msgs::OmniButtonEvent &haptic_button)
+        {
+            publish_cmd_ = (bool) haptic_button.grey_button;
+        }
+
+        void hapticJointStateCallback(const sensor_msgs::JointStateConstPtr &haptic_joint_state)
         {
             // Estimate mapping joint targets
             JointMap::iterator jm = joint_map_.find("base");
             if (jm != joint_map_.end())
             {
                 scorbot_shadow_js_.position[0] = jm->second->map(haptic_joint_state->position[0]);
-                ROS_INFO_STREAM_THROTTLE(0.1, scorbot_shadow_js_.position[0]);
+                ROS_DEBUG_STREAM_THROTTLE(0.1, scorbot_shadow_js_.position[0]);
             }
 
             // Publish shadow robot joint state
             scorbot_shadow_js_.header.stamp = ros::Time::now();
             scorbot_shadow_pub_.publish(scorbot_shadow_js_);
+
+            // Update robot command
+            cmd_.data = scorbot_shadow_js_.position[0];
+            if (publish_cmd_) scorbot_cmd_pub_.publish(cmd_);
         }
+
+
 
 
     private:
         ros::NodeHandle nh_, nh_priv_;
-        ros::Subscriber haptic_sub_;
+        ros::Subscriber haptic_js_sub_;
+        ros::Subscriber haptic_btn_sub_;
         ros::Publisher scorbot_shadow_pub_;
+        ros::Publisher scorbot_cmd_pub_;
         sensor_msgs::JointState scorbot_shadow_js_;
+        bool publish_cmd_;
+        std_msgs::Float64 cmd_;
         std::string name_;
         typedef std::map<std::string, LinearJointMappingPtr> JointMap;
         JointMap joint_map_;
