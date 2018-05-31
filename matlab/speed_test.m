37fb00937dc57ee577d0801b3bfd4ce028e2c459
Rodrigo Munoz <rorro.mr@gmail.com>
2018-05-31 03:54:30 -0400

    Add current limitation sung dynamic reconfigure

--- matlab/speed_test.m
+++ matlab/speed_test.m
@@ -5,6 +5,16 @@ RAD2ENC = 1.0/(2.0*pi/(3.0*160.0*96.0));
 Ts = 1e-3;
 Thf = 1e-6;
 Nr = 96;
+% Speed arrays
+speed_rad = diff(pos)/Ts;
+speed_rps = speed_rad/(2*pi);
+speed_rpm = speed_rps*60;
+plot(time(2:end),speed_rpm,'r');
+legend('Velocidad angular');
+xlabel('Tiempo (s)');
+ylabel('Velocidad (rpm)');
+%% Max calculation
+
 % Max speed with backward model
 max_speed_rads = max(diff(pos))/Ts
 max_speed_rps = max_speed_rads/(2*pi)
@@ -15,3 +25,9 @@ Tenc = 1/(max_speed_rps*RAD2ENC)
 ratio = Tenc/Thf
 error = Nr*max_speed_rads*Thf/(2*pi)
 
+%% Loop control data
+% speed measurement at each control loop sample
+w_rad = 2*pi/(Nr*Ts);
+w_rps = w_rad/(2*pi);
+w_rpm = w_rps*60
+w_joint = w_rpm/(3.0*160.0)
--- ros/scorbot_driver/include/scorbot_driver/scorbot_joint_driver.h
+++ ros/scorbot_driver/include/scorbot_driver/scorbot_joint_driver.h
@@ -1,7 +1,15 @@
 #ifndef SCORBOT_DRIVER_SCORBOT_JOINT_DRIVER_H
 #define SCORBOT_DRIVER_SCORBOT_JOINT_DRIVER_H
 
+#include <realtime_tools/realtime_buffer.h>
+
 #include "scorbot_driver/ethercat_driver.h"
+#include "scorbot_driver/MotorConfig.h"
+
+// Dynamic reconfigure
+#include <dynamic_reconfigure/server.h>
+#include <control_toolbox/ParametersConfig.h>
+#include <boost/thread/mutex.hpp>
 
 namespace scorbot_driver
 {
@@ -28,7 +36,9 @@ namespace scorbot_driver
     public:
         ScorbotJointDriver(const std::string& joint_name):
                 EthercatDriver(),
-                _joint_name(joint_name)
+                _joint_name(joint_name),
+                _dynamic_reconfig_initialized(false)
+
         {
 
           _name = joint_name + "_controller";
@@ -46,6 +56,18 @@ namespace scorbot_driver
           _joint_data.encSpeed = 0;
           _joint_data.current = 0;
           _joint_data.limits = 0U;
+
+          // Create node handle for dynamic reconfigure
+          ros::NodeHandle nh(_joint_name);
+          ROS_DEBUG_STREAM_NAMED(_joint_name,"Initializing dynamic reconfigure in namespace " << nh.getNamespace());
+
+          // Start dynamic reconfigure server
+          _param_reconfig_server.reset(new DynamicReconfigServer(_param_reconfig_mutex, nh));
+          _dynamic_reconfig_initialized = true;
+
+          // Set callback
+          _param_reconfig_callback = boost::bind(&ScorbotJointDriver::dynamicReconfigCallback, this, _1, _2);
+          _param_reconfig_server->setCallback(_param_reconfig_callback);
         };
 
         void setPosition(float target)
@@ -66,6 +88,8 @@ namespace scorbot_driver
 
         void update()
         {
+          // Get from RT buffers
+          _set_point.currentLim = *_current_limit_.readFromRT();
           /* Get joint data */
           _joint_data = *((joint_data_t*)(_datap->inputs));
           /* Apply set point */
@@ -78,10 +102,36 @@ namespace scorbot_driver
           return _joint_name;
         }
 
+        void dynamicReconfigCallback(scorbot_driver::MotorConfig &config, uint32_t level)
+        {
+          ROS_DEBUG_STREAM_NAMED(_joint_name,"Dynamics reconfigure callback recieved.");
+
+          if(!_dynamic_reconfig_initialized)
+              return;
+
+          // Update values using RT buffer
+          ROS_DEBUG_STREAM_NAMED(_joint_name,"Setting current_limit at " << config.current_limit);
+          _current_limit_.writeFromNonRT(std::floor(config.current_limit));
+
+
+          // Set values back using a shared mutex with dynamic reconfig
+          _param_reconfig_mutex.lock();
+          _param_reconfig_server->updateConfig(config);
+          _param_reconfig_mutex.unlock();
+        }
+
     private:
         std::string _joint_name;
         setpoint_t _set_point;
         joint_data_t _joint_data;
+        // Current limit
+        realtime_tools::RealtimeBuffer<int16_t> _current_limit_;
+        // Dynamics reconfigure
+        bool _dynamic_reconfig_initialized;
+        typedef dynamic_reconfigure::Server<scorbot_driver::MotorConfig> DynamicReconfigServer;
+        boost::shared_ptr<DynamicReconfigServer> _param_reconfig_server;
+        DynamicReconfigServer::CallbackType _param_reconfig_callback;
+        boost::recursive_mutex _param_reconfig_mutex;
     };
 
     typedef boost::shared_ptr<ScorbotJointDriver> ScorbotJointDriverPtr;
