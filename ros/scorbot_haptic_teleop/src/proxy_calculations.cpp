a12dcae9143c7fb79ace1e5a7508980de43e7cd2
Rodrigo Muñoz <rorro.mr@gmail.com>
2019-06-28 04:55:45 +0000

    Add contact movement

--- ros/scorbot_haptic_teleop/src/proxy_calculations.cpp
+++ ros/scorbot_haptic_teleop/src/proxy_calculations.cpp
@@ -114,21 +114,8 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr sampleSolidPrimitive(const shape_msgs::Solid
 
 
 class HapticProxy {
-    // Radio
-    double r1, r2, r3;
-    // Octree
-    pcl::octree::OctreePointCloudSearch<pcl::PointXYZ>::Ptr octree;
-    pcl::PointXYZ proxy_position;
-    // For store octree results
-    std::vector<int> point_idx_r1;
-    std::vector<int> point_idx_r2;
-    std::vector<int> point_idx_r3;
-    std::vector<float> point_distance_r1;
-    std::vector<float> point_distance_r2;
-    std::vector<float> point_distance_r3;
 
-    std::string frame_id;
-
+public:
     struct ProxyState {
         typedef enum
         {
@@ -138,21 +125,29 @@ class HapticProxy {
             PC_FREE
         } Type;
     };
+    struct HipState {
+        typedef enum
+        {
+            INSIDE,
+            OUTSIDE,
+            UNDEFINED
+        } Type;
+    };
 
-    ProxyState::Type state;
-public:
     HapticProxy(const double r1, const double r2, const double r3, const std::string& frame_id)
     {
       double octree_resolution = 512.0;
       octree.reset(new pcl::octree::OctreePointCloudSearch<pcl::PointXYZ>(octree_resolution));
-      // Set initial position for Proxy
-      proxy_position.x = 0.0f;
-      proxy_position.y = 0.0f;
-      proxy_position.z = 0.0f;
+      // Set initial position for Proxy and HIP
+      hip_position = Eigen::Vector3f::Zero();
+      proxy_position = Eigen::Vector3f::Zero();
       // Set initial state as Free
-      state = ProxyState::FREE;
+      proxy_state = ProxyState::FREE;
+      // Set inital state for hip as Outside
+      hip_state = HipState::OUTSIDE;
       setRadius(r1, r2, r3);
       this->frame_id = frame_id;
+      valid_normal = false;
     }
 
     void setRadius(const double r1, const double r2, const double r3)
@@ -178,7 +173,7 @@ public:
      * Update point cloud
      * @param cloud Input point cloud, must be converted to Haptic proxy frame
      */
-    void updatePointCloud(const pcl::PointCloud<pcl::PointXYZ>::Ptr& cloud)
+    void updatePointCloud(const pcl::PointCloud<pcl::PointXYZ>::Ptr cloud)
     {
       // Delete previous information and regenerate octree based on new point cloud
       octree->deleteTree();
@@ -186,20 +181,106 @@ public:
       octree->addPointsFromInputCloud();
     }
 
-    void setProxyPosition(const double x, const double y, const double z)
+    void setHipPosition(const double x, const double y, const double z)
     {
       // Set position for Proxy
-      proxy_position.x = x;
-      proxy_position.y = y;
-      proxy_position.z = z;
+      Eigen::Vector3f hip_position_t0 = hip_position;
+      hip_position.x() = x;
+      hip_position.y() = y;
+      hip_position.z() = z;
+      // Update Hip position
+      Eigen::Vector3f v = hip_position - proxy_position;
+      float delta = (hip_position - hip_position_t0).norm();
+      ROS_INFO_STREAM("Delta " << delta);
+      ROS_INFO_STREAM("v " << v.x() << ", " <<  v.y() << ", "  << v.z());
+      // Avoid update if delta it's less than a mm
+      if(fabs(delta)< 0.0005) return;
+      // Update normal
+      updateNormal();
+      if(proxy_state == ProxyState::PC_FREE || proxy_state == ProxyState::FREE)
+      {
+        Eigen::Vector3f vhat = v.normalized();
+        ROS_INFO_STREAM("vhat " << vhat.x() << ", " << vhat.y() << ", " << vhat.z());
+        Eigen::Vector3f movement = delta * vhat;
+        ROS_INFO_STREAM("Movement " << movement.x() << ", " << movement.y() << ", " << movement.z());
+        proxy_position += movement;
+        return;
+      }
+      else if(proxy_state == ProxyState::ENTRENCHMENT)
+      {
+        if(valid_normal)
+        {
+          ROS_INFO_STREAM("normal " << normal.x() << ", " << normal.y() << ", " << normal.z());
+          Eigen::Vector3f movement = delta * normal;
+          ROS_INFO_STREAM("Movement " << movement.x() << ", " << movement.y() << ", " << movement.z());
+          proxy_position += movement;
+        }
+        return;
+      }
+      else if(proxy_state == ProxyState::CONTACT)
+      {
+        if(valid_normal)
+        {
+          updateHipState();
+          if (hip_state == HipState::OUTSIDE)
+          {
+            Eigen::Vector3f vhat = v.normalized();
+            ROS_INFO_STREAM("vhat " << vhat.x() << ", " << vhat.y() << ", " << vhat.z());
+            Eigen::Vector3f movement = delta * vhat;
+            ROS_INFO_STREAM("Movement " << movement.x() << ", " << movement.y() << ", " << movement.z());
+            proxy_position += movement;
+            return;
+          }
+          else if (hip_state == HipState::INSIDE)
+          {
+            // Estimate v_plane (normal it's normalized, so n dot n = 1)
+            Eigen::Vector3f vplane = v - v.dot(normal)*normal;
+            ROS_INFO_STREAM("vplane " << vplane.x() << ", " << vplane.y() << ", " << vplane.z());
+            Eigen::Vector3f movement = delta * vplane;
+            ROS_INFO_STREAM("Movement " << movement.x() << ", " << movement.y() << ", " << movement.z());
+            proxy_position += movement;
+            return;
+
+          }
+        }
+        return;
+      }
     }
+
+    bool updateHipState()
+    {
+      // Get plane from normal and centroid
+      // From model ax + by + cz + d = 0 => d = -(ax + by + cz) = - normal dot p
+      float d = - normal.dot(centroid);
+      ROS_INFO_STREAM("d " << d);
+      // From http://mathworld.wolfram.com/Point-PlaneDistance.html
+      float D = normal.dot(hip_position) + d;
+      ROS_INFO_STREAM("D " << D);
+      // Normal and hip position are in the same side of the plane => Outside
+      hip_state = D > 0.0f ? HipState::OUTSIDE : HipState::INSIDE;
+      return true;
+    }
+
 
     void getProxyPosition(geometry_msgs::PoseStamped& pose) const
     {
       pose.header.frame_id = frame_id;
-      pose.pose.position.x = proxy_position.x;
-      pose.pose.position.y = proxy_position.y;
-      pose.pose.position.z = proxy_position.z;
+      pose.pose.position.x = proxy_position.x();
+      pose.pose.position.y = proxy_position.y();
+      pose.pose.position.z = proxy_position.z();
+      // Default orientation
+      pose.pose.orientation.w = 1.0;
+      pose.pose.orientation.x = 0.0;
+      pose.pose.orientation.y = 0.0;
+      pose.pose.orientation.z = 0.0;
+    }
+
+    void getHipPosition(geometry_msgs::PoseStamped& pose) const
+    {
+      pose.header.frame_id = frame_id;
+      pose.pose.position.x = hip_position.x();
+      pose.pose.position.y = hip_position.y();
+      pose.pose.position.z = hip_position.z();
       // Default orientation
       pose.pose.orientation.w = 1.0;
       pose.pose.orientation.x = 0.0;
@@ -217,11 +298,16 @@ public:
       point_distance_r2.clear();
       point_distance_r3.clear();
       // Get values from octree
-      octree->radiusSearch(proxy_position, r1, point_idx_r1, point_distance_r1);
+      pcl::PointXYZ proxy_position_pcl;
+      proxy_position_pcl.x = proxy_position.x();
+      proxy_position_pcl.y = proxy_position.y();
+      proxy_position_pcl.z = proxy_position.z();
+      ROS_ERROR_STREAM("Updated proxy position " << proxy_position.x() << ", " <<  proxy_position.y() << ", "  << proxy_position.z());
+      octree->radiusSearch(proxy_position_pcl, r1, point_idx_r1, point_distance_r1);
       ROS_INFO_STREAM("point_idx_r1.size() = "  << point_idx_r1.size());
-      octree->radiusSearch(proxy_position, r2, point_idx_r2, point_distance_r2);
+      octree->radiusSearch(proxy_position_pcl, r2, point_idx_r2, point_distance_r2);
       ROS_INFO_STREAM("point_idx_r2.size() = "  << point_idx_r2.size());
-      octree->radiusSearch(proxy_position, r3, point_idx_r3, point_distance_r3);
+      octree->radiusSearch(proxy_position_pcl, r3, point_idx_r3, point_distance_r3);
       ROS_INFO_STREAM("point_idx_r3.size() = "  << point_idx_r3.size());
     }
 
@@ -230,20 +316,57 @@ public:
 
       // Check for free state, no points in bigger sphere
       if(point_idx_r3.size() == 0)
-        state = ProxyState::PC_FREE;
+        proxy_state = ProxyState::PC_FREE;
       else if(point_idx_r2.size() == 0)
-        state = ProxyState::FREE;
+        proxy_state = ProxyState::FREE;
       else if(point_idx_r2.size() != 0 && point_idx_r1.size() == 0)
-        state = ProxyState::CONTACT;
+        proxy_state = ProxyState::CONTACT;
       else if(point_idx_r1.size() != 0)
-        state = ProxyState::ENTRENCHMENT;
-      ROS_INFO_STREAM("Current status: " << getStateName());
+        proxy_state = ProxyState::ENTRENCHMENT;
+      ROS_INFO_STREAM("Current status: " << getProxyStateName());
     }
 
-    std::string getStateName()
+    bool updateNormal()
+    {
+      if (point_idx_r3.size() <= 3)
+      {
+        ROS_INFO("No enough points to estimate normal");
+        valid_normal = false;
+        return valid_normal;
+      }
+      pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud =  octree->getInputCloud();
+      Eigen::Vector3f vector_sum = Eigen::Vector3f::Zero();
+      // Set to zero
+      normal = Eigen::Vector3f::Zero();
+      centroid = Eigen::Vector3f::Zero();
+      for(std::size_t i=0; i < point_idx_r3.size(); ++i)
+      {
+        // Normal calculation
+        Eigen::Vector3f pk = cloud->points[point_idx_r3[i]].getVector3fMap();
+        Eigen::Vector3f diff = proxy_position - pk;
+        float norm = diff.norm();
+        vector_sum += 1.0/norm*diff;
+        // Average point in the point cloud
+        centroid += pk;
+      }
+      normal = 1.0/point_idx_r3.size()*vector_sum;
+      centroid = 1.0/point_idx_r3.size()*centroid;
+      this->normal = normal.normalized();
+      this->centroid = centroid;
+      valid_normal = true;
+      return valid_normal;
+    }
+
+    bool getNormal(Eigen::Vector3f &normal) const
+    {
+      normal = this->normal;
+      return valid_normal;
+    }
+
+    std::string getProxyStateName() const
     {
       std::string state_name;
-      switch (state)
+      switch (proxy_state)
       {
         case ProxyState::PC_FREE:
           state_name = "PC_FREE";
@@ -259,6 +382,56 @@ public:
       }
       return state_name;
     }
+
+    HipState::Type getHipState() const
+    {
+      return hip_state;
+    }
+
+    std::string getHipStateName() const
+    {
+      std::string state_name;
+      switch (hip_state)
+      {
+        case HipState::OUTSIDE:
+          state_name = "OUTSIDE";
+          break;
+        case HipState::INSIDE:
+          state_name = "INSIDE";
+          break;
+        case HipState::UNDEFINED:
+          state_name = "UNDEFINED";
+          break;
+      }
+      return state_name;
+    }
+
+    ProxyState::Type getProxyState() const
+    {
+      return proxy_state;
+    }
+private:
+    // Radio
+    double r1, r2, r3;
+    // Octree
+    pcl::octree::OctreePointCloudSearch<pcl::PointXYZ>::Ptr octree;
+    Eigen::Vector3f proxy_position;
+    Eigen::Vector3f hip_position;
+    Eigen::Vector3f normal;
+    Eigen::Vector3f centroid;
+    bool valid_normal;
+    // For store octree results
+    std::vector<int> point_idx_r1;
+    std::vector<int> point_idx_r2;
+    std::vector<int> point_idx_r3;
+    std::vector<float> point_distance_r1;
+    std::vector<float> point_distance_r2;
+    std::vector<float> point_distance_r3;
+
+    std::string frame_id;
+
+    ProxyState::Type proxy_state;
+    HipState::Type hip_state;
 };
 typedef boost::shared_ptr<HapticProxy> HapticProxyPtr;
 
@@ -266,7 +439,7 @@ typedef boost::shared_ptr<HapticProxy> HapticProxyPtr;
 class HapticProxyMarker
 {
     // Markers
-    visualization_msgs::Marker r1_marker, r2_marker, r3_marker, state_marker;
+    visualization_msgs::Marker r1_marker, r2_marker, r3_marker, state_marker, normal_marker;
     HapticProxyPtr proxy;
     ros::NodeHandle nh;
     ros::Publisher marker_pub;
@@ -281,26 +454,39 @@ class HapticProxyMarker
     void updateProxyMarker()
     {
       // Get proxy position
-      geometry_msgs::PoseStamped proxy_pose;
+      geometry_msgs::PoseStamped proxy_pose, hip_pose;
       proxy->getProxyPosition(proxy_pose);
+      proxy->getHipPosition(hip_pose);
       // Sphere markers
       r1_marker.type = visualization_msgs::Marker::SPHERE;
       r2_marker.type = visualization_msgs::Marker::SPHERE;
       r3_marker.type = visualization_msgs::Marker::SPHERE;
       state_marker.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
+      normal_marker.type = visualization_msgs::Marker::ARROW;
       // Add option
       r1_marker.action = visualization_msgs::Marker::ADD;
       r2_marker.action = visualization_msgs::Marker::ADD;
       r3_marker.action = visualization_msgs::Marker::ADD;
       state_marker.action = visualization_msgs::Marker::ADD;
+      normal_marker.action = visualization_msgs::Marker::ADD;
       //  Update marker pose
-      r1_marker.pose.position = proxy_pose.pose.position;
-      r1_marker.pose.orientation = proxy_pose.pose.orientation;
+      r1_marker.pose.position = hip_pose.pose.position;
+      r1_marker.pose.orientation = hip_pose.pose.orientation;
       r2_marker.pose.position = proxy_pose.pose.position;
       r3_marker.pose.orientation = proxy_pose.pose.orientation;
       r3_marker.pose.position = proxy_pose.pose.position;
       r3_marker.pose.orientation = proxy_pose.pose.orientation;
       state_marker.pose.position = proxy_pose.pose.position;
+      geometry_msgs::Point normal_marker_start, normal_marker_end;
+      normal_marker_start = proxy_pose.pose.position;
+      Eigen::Vector3f normal;
+      bool valid_normal = proxy->getNormal(normal);
+      normal *= 0.1;
+      normal_marker_end.x = normal.x(); normal_marker_end.y = normal.y();  normal_marker_end.z = normal.z();
+      normal_marker.points.clear();
+      normal_marker.points.push_back(normal_marker_start);
+      normal_marker.points.push_back(normal_marker_end);
+
 
       // Set the scale of the markers
       double scale_1, scale_2, scale_3;
@@ -314,11 +500,14 @@ class HapticProxyMarker
       r3_marker.scale.x = scale_3*2.0;
       r3_marker.scale.y = scale_3*2.0;
       r3_marker.scale.z = scale_3*2.0;
+      normal_marker.scale.x = 0.01;
+      normal_marker.scale.y = 0.02;
+      normal_marker.scale.z = 0.01;
       // Status text
-      state_marker.text = proxy->getStateName();
-      state_marker.scale.x = 0.05;
-      state_marker.scale.y = 0.05;
-      state_marker.scale.z = 0.05;
+      state_marker.text = proxy->getProxyStateName();
+      state_marker.scale.x = 0.010;
+      state_marker.scale.y = 0.020;
+      state_marker.scale.z = 0.015;
       state_marker.pose.position.z += scale_3+0.05;
       // Set the color and alpha
       r1_marker.color.r = 1.00f;
@@ -340,6 +529,16 @@ class HapticProxyMarker
       state_marker.color.g = 1.0f;
       state_marker.color.b = 1.0f;
       state_marker.color.a = 1.0f;
+
+      normal_marker.color.r = 1.0f;
+      normal_marker.color.g = 0.0f;
+      normal_marker.color.b = 0.0f;
+      normal_marker.color.a = 1.0f;
+      if (!valid_normal)
+      {
+        normal_marker.action = visualization_msgs::Marker::DELETE;
+        normal_marker.color.a = 0.0f;
+      }
       // Set id and namespace
       r1_marker.id = 1;
       r1_marker.ns = "proxy_marker/r1";
@@ -349,6 +548,8 @@ class HapticProxyMarker
       r3_marker.ns = "proxy_marker/r3";
       state_marker.id = 4;
       state_marker.ns = "proxy_marker/state";
+      normal_marker.id = 5;
+      state_marker.ns = "proxy_marker/normal";
       // Update header
       ros::Time now = ros::Time::now();
       r1_marker.header.frame_id = proxy_pose.header.frame_id;
@@ -359,24 +560,33 @@ class HapticProxyMarker
       r3_marker.header.stamp = now;
       state_marker.header.frame_id = proxy_pose.header.frame_id;
       state_marker.header.stamp = now;
+      normal_marker.header.frame_id = proxy_pose.header.frame_id;
+      normal_marker.header.stamp = now;
       // Publish Markers
       visualization_msgs::MarkerArray marker;
       marker.markers.push_back(r1_marker);
       marker.markers.push_back(r2_marker);
       marker.markers.push_back(r3_marker);
       marker.markers.push_back(state_marker);
+      marker.markers.push_back(normal_marker);
       marker_pub.publish(marker);
     }
-
-
 };
+
+
+geometry_msgs::PoseStamped target_hip_pose;
+void poseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg)
+{
+  target_hip_pose = *msg;
+  ROS_ERROR_STREAM("Updated position " << msg->pose.position.x << ", " <<  msg->pose.position.y << ", "  << msg->pose.position.z);
+}
 
 int main (int argc, char** argv){
   shape_msgs::SolidPrimitive sample;
   sample.type  = shape_msgs::SolidPrimitive::BOX;
-  sample.dimensions.push_back(0.1);
-  sample.dimensions.push_back(0.2);
-  sample.dimensions.push_back(0.05);
+  sample.dimensions.push_back(0.4);
+  sample.dimensions.push_back(0.4);
+  sample.dimensions.push_back(0.03);
 
   // Initialize ROS
   ros::init (argc, argv, "proxy_test");
@@ -384,7 +594,17 @@ int main (int argc, char** argv){
   ros::Publisher pub = nh.advertise<sensor_msgs::PointCloud2>("output", 1);
   ros::Rate rate(30);
 
-  HapticProxyPtr proxy = boost::make_shared<HapticProxy>(0.01, 0.02, 0.03, "proxy");
+  target_hip_pose.pose.position.x = 0.0;
+  target_hip_pose.pose.position.y = 0.0;
+  target_hip_pose.pose.position.z = 0.1;
+  target_hip_pose.pose.orientation.x = 0.0;
+  target_hip_pose.pose.orientation.y = 0.0;
+  target_hip_pose.pose.orientation.z = 0.0;
+  target_hip_pose.pose.orientation.w = 1.0;
+  target_hip_pose.header.frame_id = "world";
+  ros::Subscriber sub = nh.subscribe("/marker_pose", 1, poseCallback);
+
+  HapticProxyPtr proxy = boost::make_shared<HapticProxy>(0.01, 0.02, 0.03, "world");
   HapticProxyMarker proxy_marker(proxy, nh);
 
   double roll = 0.0, pitch = 0, yaw = 0.0;
@@ -419,8 +639,15 @@ int main (int argc, char** argv){
       proxy->updatePointCloud(point_cloud_proxy);
       proxy->updateProxyPoints();
       proxy->updateProxyState();
+      proxy->setHipPosition(target_hip_pose.pose.position.x, target_hip_pose.pose.position.y, target_hip_pose.pose.position.z);
+      if (proxy->getProxyState() != HapticProxy::ProxyState::PC_FREE)
+      {
+        Eigen::Vector3f normal;
+        bool normal_validation = proxy->updateNormal();
+      }
     }
     proxy_marker.updateProxyMarker();
+    ros::spinOnce();
     rate.sleep();
   }
 }
--- ros/scorbot_haptic_teleop/src/scorbot_haptic_teleop/tf_marker.py
+++ ros/scorbot_haptic_teleop/src/scorbot_haptic_teleop/tf_marker.py
@@ -5,41 +5,35 @@
 TF Interactive marker
 """
 
-__author__ = 'Rodrigo Muñoz'
+__author__ = "Rodrigo Muñoz"
 
 import rospy
 from interactive_markers.interactive_marker_server import InteractiveMarkerServer
 from visualization_msgs.msg import (InteractiveMarkerControl, InteractiveMarker,
                                     InteractiveMarkerFeedback)
-from tf.broadcaster import TransformBroadcaster
+from geometry_msgs.msg import PoseStamped
 from threading import Lock
 import argparse
 
 class TFMarkerServer(object):
     """TFMarkerServer"""
-    def __init__(self, rate = 30.0, init_position = (0.0, 0.0, 0.0), parent_frame = "wolrd", child_frame = "tf_marker",
-                 scale=0.3):
+    def __init__(self, rate = 30.0, init_position = (0.0, 0.0, 0.0), parent_frame = "world", scale=0.3):
         # Marker server
-        self.server = InteractiveMarkerServer('tf_marker')
+        self.server = InteractiveMarkerServer("tf_marker")
         rospy.loginfo("Starting TF marker")
-        rospy.loginfo("Publish transform from {} to {} at rate {}".format(parent_frame, child_frame, rate))
+        rospy.loginfo("Publish pose from frame_id {} at rate {}".format(parent_frame, rate))
         rospy.loginfo("Initial position: {}".format(init_position))
-        # TF broadcaster
-        self.tf = TransformBroadcaster()
-
         # Marker pose
         self.pose_mutex = Lock()
         self.marker_position = init_position
         self.marker_orientation = (0.0, 0.0, 0.0, 1.0)
         self.scale = scale
-
         # Frames
         self.parent_frame = parent_frame
-        self.child_frame = child_frame
-
+        # Pose publisher
+        self.pub = rospy.Publisher('marker_pose', PoseStamped, queue_size=1)
         # Add marker
         self.add_6DOF()
-
         # Timer for TF broadcaster
         self.rate = rate
         rospy.Timer(rospy.Duration(1.0/self.rate), self.publish_transform)
@@ -51,8 +45,8 @@ class TFMarkerServer(object):
         marker.pose.orientation.x,marker.pose.orientation.y,marker.pose.orientation.z,marker.pose.orientation.w = self.marker_orientation
         marker.scale = self.scale
 
-        marker.name = 'tf_marker'
-        marker.description = '6-DOF pose control'
+        marker.name = "tf_marker"
+        marker.description = ""
 
         # X axis rotation
         control = InteractiveMarkerControl()
@@ -115,15 +109,20 @@ class TFMarkerServer(object):
     def publish_transform(self, timer_event):
         time = rospy.Time.now() + rospy.Duration(1.0/self.rate)
         self.pose_mutex.acquire()
-        self.tf.sendTransform(self.marker_position, self.marker_orientation,
-                              time, self.child_frame, self.parent_frame)
+        current_pose = PoseStamped()
+        current_pose.header.stamp = rospy.Time.now()
+        current_pose.header.frame_id = self.parent_frame
+        current_pose.pose.position.x, current_pose.pose.position.y, current_pose.pose.position.z = self.marker_position
+        current_pose.pose.orientation.x, current_pose.pose.orientation.y, current_pose.pose.orientation.z, \
+            current_pose.pose.orientation.w = self.marker_orientation
+        self.pub.publish(current_pose)
         self.pose_mutex.release()
 
     def marker_feedback(self, feedback):
-        rospy.loginfo('Feedback from ' + feedback.marker_name)
+        rospy.logdebug("Feedback from " + feedback.marker_name)
         # Check event
         if feedback.event_type == InteractiveMarkerFeedback.POSE_UPDATE:
-            rospy.loginfo( 'Pose changed')
+            rospy.loginfo("Pose changed to {}".format(feedback.pose.position))
             # Update marker position
             self.pose_mutex.acquire()
             self.marker_position = (feedback.pose.position.x,
@@ -136,17 +135,15 @@ class TFMarkerServer(object):
 
 
 def main():
-    rospy.init_node('tf_marker')
+    rospy.init_node("tf_marker")
     parser = argparse.ArgumentParser("Publish TF based on position of 6DOF marker")
-    parser.add_argument('--position', nargs='+', dest='position', required=True)
-    parser.add_argument('--parent_frame', dest='parent_frame', required=True)
-    parser.add_argument('--child_frame', dest='child_frame', required=True)
-    parser.add_argument('--rate', dest='rate', default=30.0)
-    parser.add_argument('--scale', dest='scale', default=0.3)
+    parser.add_argument("--position", nargs="+", dest="position", required=True)
+    parser.add_argument("--parent_frame", dest="parent_frame", required=True)
+    parser.add_argument("--rate", dest="rate", default=30.0)
+    parser.add_argument("--scale", dest="scale", default=0.3)
     args = parser.parse_args()
     init_position = tuple(float(i) for i in args.position)
-    marker_server = TFMarkerServer(rate=args.rate, init_position=init_position, parent_frame=args.parent_frame,
-                                   child_frame=args.child_frame, scale=float(args.scale))
+    marker_server = TFMarkerServer(rate=args.rate, init_position=init_position, parent_frame=args.parent_frame, scale=float(args.scale))
     rospy.spin()
 
 
